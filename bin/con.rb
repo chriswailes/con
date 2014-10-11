a4c9eb86e3212f06848930e1cb2b644bddcfface
Dan Albert <dan@gingerhq.net>
2014-10-10 22:41:05 -0700

    Start building a driver around con.
    Con can now run a few different ways:
    
        # launch the REPL
        $ con.rb # or con.rb --repl
    
        # execute (foo)
        $ echo '(foo)' | con.rb
    
        # execute the contents of foo
        $ con.rb -f foo
    
        # dump the token stream of (foo)
        $ con.rb --tokenize
        (foo)^D
    
        # tokenize the contents of foo
        $ con.rb --tokenize -f foo
    
        # dump the ast of (foo)
        $ con.rb --dumpast
        (foo)^D
    
        # parse the contents of foo and dump the AST
        $ con.rb --dumpast -f foo

--- bin/con.rb
+++ bin/con.rb
@@ -7,29 +7,97 @@ require 'readline'
 require 'con/lexer'
 require 'con/parser'
 require 'con/passes/type_checking'
+require 'con/version'
 
-continue = true
+require 'filigree/configuration'
+require 'filigree/match'
 
-while continue
-	line = Readline.readline("Con > ", true)
+class Configuration
+	include Filigree::Configuration
 
+	attr_reader :mode
 
-	if not line or line == 'exit'
-		puts
-		continue = false
-	else
-		begin
-			ast = Con::Parser.parse(Con::Lexer.lex(line))
-
-			Con::TypeChecking.new.visit ast
-
-			puts ast.to_s
-
-		rescue RLTK::NotInLanguage => e
-			puts 'Invalid input'
-
-		rescue Con::UnknownVariable => e
-			puts e.message
+	def set_mode(mode)
+		if not @mode.nil?
+			raise 'Only one mode may be used.'
 		end
+		@mode = mode
 	end
+
+	help 'Dump lexer output and exit.'
+	option 'tokenize' do
+		set_mode :lex
+	end
+
+	help 'Dump AST and exit.'
+	option 'dumpast' do
+		set_mode :parse
+	end
+
+	help 'Launch the REPL.'
+	option 'repl' do
+		set_mode :repl
+	end
+
+	help 'File to process.'
+	string_option 'file', 'f'
+end
+
+def file_contents_or_stdin(file)
+	if file then File.open(file).read else STDIN.read end
+end
+
+def tokenize(conf)
+	puts Con::Lexer.lex(file_contents_or_stdin conf.file)
+end
+
+def dump_ast(conf)
+	puts Con::Parser.parse(Con::Lexer.lex(file_contents_or_stdin(conf.file)))
+end
+
+def evaluate(contents)
+	begin
+		ast = Con::Parser.parse(Con::Lexer.lex(contents))
+
+		Con::TypeChecking.new.visit ast
+
+		puts ast.to_s
+
+	rescue RLTK::NotInLanguage => e
+		puts 'Invalid input'
+
+	rescue Con::UnknownVariable => e
+		puts e.message
+	end
+end
+
+def repl(conf)
+	# TODO: If a file was given, its contents should be sourced into the
+	# environment before the REPL loads.
+	puts "Con v#{Con::VERSION} REPL (CTRL-D to exit)"
+	loop {
+		line = Readline.readline("Con > ", true)
+		if not line
+			puts
+			break
+		else
+			evaluate line
+		end
+	}
+end
+
+conf = Configuration.new(ARGV)
+match conf.mode do
+	with(:lex) { tokenize conf }
+	with(:parse) { dump_ast conf }
+	with(:repl) { repl conf }
+	with(_) {
+		if conf.file
+			evaluate File.open(conf.file).read
+		elsif STDIN.isatty
+			repl conf
+		else
+			evaluate STDIN.read
+		end
+	}
 end
